0bdb5ba806ff3381f8d727597855660f1f5d9a82
faizttn <faijuddin@tothenew.com>
2024-01-22 22:09:43 +0530

    cretaed jwt-passport guard

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -1,13 +1,17 @@
 import {
   Body,
   Controller,
+  Get,
   HttpException,
   Post,
+  Req,
   UseGuards,
 } from '@nestjs/common';
 import { AuthPayloadDto } from './dto/auth.dto';
 import { AuthService } from './auth.service';
 import { AuthGuard } from '@nestjs/passport';
+import { Request } from 'express';
+import { JwtAuthGuard } from './guards/jwt.guard';
 
 @Controller('auth')
 export class AuthController {
@@ -21,5 +25,12 @@ export class AuthController {
     // if (!user) throw new HttpException('Invalid credentials', 401);
 
     return user;
+  }
+
+  @Get('status')
+  @UseGuards(JwtAuthGuard)
+  status(@Req() req: Request) {
+    console.log('inside auth controler', req.user);
+    return req.user
   }
 }
