2c213c7c3b43642f08a9c2aa8cfadd4f1eb8fda9
faizttn <faijuddin@tothenew.com>
2024-01-22 21:39:23 +0530

    auth guard created

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -1,13 +1,25 @@
-import { Body, Controller, Post } from '@nestjs/common';
+import {
+  Body,
+  Controller,
+  HttpException,
+  Post,
+  UseGuards,
+} from '@nestjs/common';
 import { AuthPayloadDto } from './dto/auth.dto';
 import { AuthService } from './auth.service';
+import { AuthGuard } from '@nestjs/passport';
 
 @Controller('auth')
 export class AuthController {
   constructor(private authService: AuthService) {}
 
   @Post('/login')
+  @UseGuards(AuthGuard('local'))
   login(@Body() authPayload: AuthPayloadDto) {
-    return this.authService.validateUser(authPayload);
+    const user = this.authService.validateUser(authPayload);
+
+    // if (!user) throw new HttpException('Invalid credentials', 401);
+
+    return user;
   }
 }
